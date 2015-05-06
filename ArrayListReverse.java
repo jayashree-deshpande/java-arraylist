3f96c4f1f207190b0699c110c18d299913252754
Jayashree Deshpande <jdeshpan@usc.edu>
2015-05-06 15:59:35 -0400

    Create ArrayListReverse.java

---
+++ ArrayListReverse.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+ 
+public class ArrayListReverse {
+ 
+    public static void main(String[] args){
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Java");
+        list.add("JQuery");
+        list.add("Python");
+        list.add("SQL");
+        list.add("C");
+        Collections.reverse(list);
+        System.out.println("Results after reverse operation:");
+        for(String str: list){
+            System.out.println(str);
+        }
+    }
+}
